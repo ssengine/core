1d41e6c70e2133d602e5522c8a9f6fb79f986cd5
Graphics <softgiant@126.com>
2015-03-08 21:25:18 +0800

    re-implement the lua interface of image.

--- src/decoders/decoder.h
+++ src/decoders/decoder.h
@@ -9,26 +9,31 @@
 extern "C" {
 #endif
 
-typedef struct ss_userdata_image {
-	ss_texture_resource_ref *pointer;
+typedef struct ss_image_data {
 	unsigned int width;
 	unsigned int height;
 	enum ss_render_format format;
 	unsigned char *pixels;
 
-	void init() {
-		pointer = nullptr;
-		width = height = 0;
-		format = SS_FORMAT_NULL;
-		pixels = nullptr;
+	ss_image_data()
+		:width(0), height(0), format(SS_FORMAT_NULL), pixels(nullptr)
+	{
+		
 	}
 
-	void gen_texture(ss_core_context *C) {
+	~ss_image_data() {
+		if (pixels != nullptr) {
+			delete[] pixels;
+			pixels = nullptr;
+		}
+	}
+
+	ss_texture2d * gen_texture(ss_core_context *C) {
 		ss_render_device *device = ss_get_render_device(C);
 		ss_texture2d *texture = device->create_texture2d(width, height, format, pixels);
-		pointer->set(texture);
 		delete[] pixels;
 		pixels = nullptr;
+		return texture;
 	}
 } ss_userdata_image;
 
--- src/modules/mod_render2d.cpp
+++ src/modules/mod_render2d.cpp
@@ -7,36 +7,35 @@
 #include "../core.h"
 #include "ssengine/render/resources.h"
 
-#define get_image(L, index) (ss_userdata_image*)lua_touserdata(L, index)
+#define get_res(L, index) (ss_userdata_resource*)lua_touserdata(L, index)
 
 static int ll_load_image(lua_State *L) {
-	ss_userdata_image *img = get_image(L, 1);
-	ss_resource_ref_impl *ref = wrap(reinterpret_cast<ss_resource_ref*>(img->pointer));
-	const char *uri = ref->uri.c_str();
-
-	ss_init_decoder();
-	if (ss_decode_image(uri, img) != 0) {
-		return luaL_error(L, "load image failed. the image is:%s", uri);
-	}
+	ss_userdata_resource *ures = get_res(L, 1);
 	ss_core_context *C = ss_lua_get_core_context(L);
-	img->gen_texture(C);
+	int result = ures->res->prototype->sync_load(C, ures->res);
+	if (result != 0) {
+		ss_resource_ref_impl *ref_impl = wrap(ures->res);
+		return luaL_error(L, "load image failed. the image is:%s", ref_impl->uri.c_str());
+	}
+	//gen texture.
+	ss_image_data *img_data = reinterpret_cast<ss_image_data*>(ures->res->extra_info_ptr);
+	ures->res->ptr = img_data->gen_texture(C);
 	return 0;
 }
 
 static int ll_unload_image(lua_State *L) {
-	ss_userdata_image *img = get_image(L, 1);
-	ss_resource_ref *ref = reinterpret_cast<ss_resource_ref*>(img->pointer);
+	ss_userdata_resource *ures = get_res(L, 1);
 	ss_core_context *C = ss_lua_get_core_context(L);
-	ref->prototype->unload(C, ref);
+	ures->res->prototype->unload(C, ures->res);
 	return 0;
 }
 
 static int ll_draw_image(lua_State *L) {
-	ss_userdata_image *img = get_image(L, 1);
-	ss_resource_ref *ref = reinterpret_cast<ss_resource_ref*>(img->pointer);
-	ss_texture2d *texture = (ss_texture2d*)ref->ptr;
+	ss_userdata_resource *ures = get_res(L, 1);
+	ss_resource_ref *ref = ures->res;
+	ss_texture2d *texture = reinterpret_cast<ss_texture2d*>(ref->ptr);
 	if (texture == nullptr) {
-		return luaL_error(L, "draw image failed cause by nil texture, maybe you should load first!");
+		return luaL_error(L, "draw image failed cause by a nil texture, maybe you should load first!");
 	}
 	ss_core_context *C = ss_lua_get_core_context(L);
 	ss_db_draw_image_rect(C, texture, -1, 1, 2, 2, 0, 0, 1, 1);
@@ -48,9 +47,10 @@ static int ll_create_image(lua_State *L) {
 	ss_core_context *C = ss_lua_get_core_context(L);
 	ss_texture_resource_ref *texture = ss_texture_resource(C, uri);
 
-	ss_userdata_image *img = (ss_userdata_image*)lua_newuserdata(L, sizeof(ss_userdata_image));
-	img->init();
-	img->pointer = texture;
+	ss_userdata_resource *ures = (ss_userdata_resource*)lua_newuserdata(L, sizeof(ss_userdata_resource));
+	ures->res = reinterpret_cast<ss_resource_ref*>(texture);
+	ss_image_data *img_data = new ss_image_data();
+	ures->res->extra_info_ptr = img_data;
 	
 	luaL_newmetatable(L, "image_meta");
 	lua_pushvalue(L, -1);
