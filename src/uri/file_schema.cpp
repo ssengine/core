94e2fb5e6c6016fd5a480cd47e4790ce8753f963
DengYun <tdzl2003@gmail.com>
2015-04-01 23:17:31 +0800

    support unicode file name (with utf-8 encoding).

--- src/uri/file_schema.cpp
+++ src/uri/file_schema.cpp
@@ -4,6 +4,8 @@
 #ifdef WIN32
 #include <sys/stat.h>
 #include <direct.h>
+#include <sys/types.h>
+#include <wchar.h>
 #else
 #include <unistd.h>
 #endif
@@ -11,19 +13,49 @@
 //TODO: support file that more than 2G
 //TODO: support chinese file name(use UNICODE API)
 //TODO: use uri.to_path() instead of path.c_str()+1
+//TODO: support path long than MAX_PATH
+
+#if WIN32
+#include <Shlwapi.h>
+#include <Wininet.h>
+
+#pragma comment(lib, "Shlwapi.lib")
+static std::wstring ss_file_uri_to_path(const ss_uri& uri){
+    wchar_t* wUri = char2wchar_t(uri.str().c_str());
+
+    wchar_t wOutPath[MAX_PATH + 1];
+    DWORD wOutSize = MAX_PATH;
+    if (S_OK != PathCreateFromUrlW(wUri, wOutPath, &wOutSize, NULL)){
+        wOutPath[0] = 0;
+    }
+
+    delete[] wUri;
+    return wOutPath;
+}
+#else
+static std::wstring ss_file_uri_to_path(const ss_uri& path){
+    ss_uri ret;
+
+    bool isAbsolute = (path[0] == '/');
+
+    return ret;
+}
+#endif
+
 
 bool ss_file_schema_handler::is_local(){
 	return true;
 }
 
 bool ss_file_schema_handler::exists(const ss_uri& uri){
-	struct stat st;
-	return stat(uri.path.c_str() + 1, &st) == 0;
+    struct _stat st;
+    return _wstat(ss_file_uri_to_path(uri).c_str() + 1, &st) == 0;
 }
 
 bool ss_file_schema_handler::is_file(const ss_uri& uri){
-	struct stat st;
-	if (stat(uri.path.c_str() + 1, &st) != 0){
+    struct _stat st;
+    if (_wstat(ss_file_uri_to_path(uri).c_str() + 1, &st) != 0)
+    {
 		return false;
 	}
 	return (st.st_mode & S_IFMT) == S_IFREG;
@@ -31,8 +63,8 @@ bool ss_file_schema_handler::is_file(const ss_uri& uri){
 
 bool ss_file_schema_handler::is_directory(const ss_uri& uri)
 {
-	struct stat st;
-	if (stat(uri.path.c_str() + 1, &st) != 0)
+    struct _stat st;
+    if (_wstat(ss_file_uri_to_path(uri).c_str() + 1, &st) != 0)
 	{
 		return false;
 	}
@@ -41,7 +73,7 @@ bool ss_file_schema_handler::is_directory(const ss_uri& uri)
 
 bool ss_file_schema_handler::mkdir(const ss_uri& uri)
 {
-	return _mkdir(uri.path.c_str() + 1) == 0;
+    return _wmkdir(ss_file_uri_to_path(uri).c_str() + 1) == 0;
 }
 
 struct file_input_stream : input_stream
@@ -132,7 +164,7 @@ size_t file_output_stream::write(const void* buf, size_t sz)
 
 struct input_stream* ss_file_schema_handler::open_for_read(const ss_uri& uri)
 {
-	FILE* f = fopen(uri.path.c_str() + 1, "rb");
+    FILE* f = _wfopen(ss_file_uri_to_path(uri).c_str(), L"rb");
 	if (f){
 		return new file_input_stream(f);
 	}
@@ -141,7 +173,7 @@ struct input_stream* ss_file_schema_handler::open_for_read(const ss_uri& uri)
 
 struct output_stream* ss_file_schema_handler::open_for_write(const ss_uri& uri)
 {
-	FILE* f = fopen(uri.path.c_str() + 1, "wb");
+    FILE* f = _wfopen(ss_file_uri_to_path(uri).c_str(), L"wb");
 	if (f){
 		return new file_output_stream(f);
 	}
@@ -150,7 +182,7 @@ struct output_stream* ss_file_schema_handler::open_for_write(const ss_uri& uri)
 
 struct output_stream* ss_file_schema_handler::open_for_append(const ss_uri& uri)
 {
-	FILE* f = fopen(uri.path.c_str() + 1, "ab");
+    FILE* f = _wfopen(ss_file_uri_to_path(uri).c_str(), L"ab");
 	if (f){
 		return new file_output_stream(f);
 	}
